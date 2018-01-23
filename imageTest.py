d8323b3ebb8e2b1d428ec7a340a8f2997759ba2c
root <uberthought@gmail.com>
2018-01-23 12:20:10 +0000

    added convolution layer

--- imageTest.py
+++ imageTest.py
@@ -2,25 +2,38 @@
 
 from PIL import Image
 import numpy as np
+import os
 
 from network import Model
 
 model = Model()
 
-image = Image.open('test.jpg')
+names = os.listdir('images')
 
-size = image.size
+X = np.array([], dtype=np.float).reshape(0, Model.size, Model.size, 3)
+Y = np.array([], dtype=np.float).reshape(0, Model.output_size)
 
-image = image.convert('RGB')
-image = image.resize((Model.size, Model.size))
-pixels = np.array(image)
+i = 0
+for name in names:
+    image = Image.open('images/' + name)
+    image = image.convert('RGB')
+    image = image.resize((Model.size, Model.size))
+    pixels = np.array(image)
 
-loss = model.train([pixels], [pixels])
+    X = np.concatenate((X, [pixels]), axis=0)
+    y = np.zeros(Model.output_size)
+    y[i] = 1
+    Y = np.concatenate((Y, [y]))
+
+    i += 1
+
+loss = model.train(X, Y)
 model.save()
-print(loss)
 
-pixels2 = model.run([pixels])[0]
-image2 = Image.fromarray(pixels2, "RGB")
-image2 = image2.resize(size)
-
-image2.save('test2.jpg')
+for i in range(X.shape[0]):
+    pixels2 = X[i:i+1:1][0]
+    result = model.run([pixels2])[0]
+    print(i, np.argmax(result), result)
+    image2 = Image.fromarray(pixels2.astype(np.int8), "RGB")
+    image2.save('test' + str(i) + '.png')
+    # image2 = image2.resize(size)
--- network.py
+++ network.py
@@ -4,53 +4,70 @@ import os.path
 import math
 import time
 
-def createHidden(input_layer, units, name):
-        input_units = int(input_layer.shape[1])
+def createHidden(input, shape, name):
+        input_units = int(np.prod(input.shape.as_list()[1:]))
+        output_units = np.prod(shape)
         with tf.name_scope(name):
-            weights = tf.Variable(tf.truncated_normal([input_units, units], stddev=1.0/math.sqrt(float(input_units)), dtype=tf.float32), name='weights')
-            biases = tf.Variable(tf.zeros([units], dtype=tf.float32), name='biases')
-            result = tf.matmul(input_layer, weights)
-            result = tf.nn.relu(result)
-            result = tf.add(result, biases)
-            if input_units == units:
-                result = tf.add(input_layer, result)
-            return result
+            weights = tf.Variable(tf.truncated_normal([input_units, output_units], stddev=1.0/math.sqrt(float(input_units)), dtype=tf.float32), name='weights')
+            biases = tf.Variable(tf.zeros([output_units], dtype=tf.float32), name='biases')
 
-def createOutput(input_layer, units, name):
-        input_units = int(input_layer.shape[1])
+            flatten = tf.reshape(input, (-1, input_units))
+            output = tf.matmul(flatten, weights)
+            output = tf.add(output, biases)
+            output = tf.nn.relu(output)
+            if input_units == output_units:
+                output = tf.add(flatten, output)
+            output = tf.reshape(output, (-1,) + shape)
+
+            return output
+
+def createOutput(input, shape, name):
+        input_units = int(np.prod(input.shape.as_list()[1:]))
+        output_units = int(np.prod(shape))
         with tf.name_scope(name):
-            weights = tf.Variable(tf.truncated_normal([input_units, units], stddev=1.0/math.sqrt(float(input_units)), dtype=tf.float32), name='weights')
-            biases = tf.Variable(tf.zeros([units], dtype=tf.float32), name='biases')
-            result = tf.matmul(input_layer, weights)
-            result = tf.add(result, biases)
-            if input_units == units:
-                result = tf.add(input_layer, result)
-            return result
+            weights = tf.Variable(tf.truncated_normal([input_units, output_units], stddev=1.0/math.sqrt(float(input_units)), dtype=tf.float32), name='weights')
+            biases = tf.Variable(tf.zeros([output_units], dtype=tf.float32), name='biases')
+
+            flatten = tf.reshape(input, (-1, input_units))
+            output = tf.matmul(flatten, weights)
+            output = tf.add(output, biases)
+            if input_units == output_units:
+                output = tf.add(flatten, output)
+            output = tf.reshape(output, (-1,) + shape)
+
+            return output
 
 class Model:
-    size=256
+    size=64
+    output_size=4
 
     def __init__(self):
 
         size = Model.size
-
-        self.X = tf.placeholder(tf.float32, shape=(None, size, size, 3), name='X')
-        self.Y = tf.placeholder(tf.float32, shape=(None, size, size, 3), name='Y')
-
-        input = tf.reshape(self.X, (-1, size * size * 3), name='input')
-        hidden1 = createHidden(input, 128, 'hidden1')
-        hidden2 = createHidden(hidden1, 32, 'hidden2')
-        hidden3 = createHidden(hidden2, 128, 'hidden3')
-        output = createOutput(hidden3, size * size * 3, 'output')
-
-        self.prediction = tf.reshape(output, (-1, size, size, 3), name='prediction')
-
-        self.loss = tf.reduce_mean(tf.losses.mean_squared_error(self.Y, self.prediction), name='loss')
-        self.run_train = tf.train.AdagradOptimizer(.1).minimize(self.loss)
-
-        self.output = tf.cast(self.prediction, tf.int8, name='output')
+        output_size = Model.output_size
+        shape = (size, size, 3)
 
         self.sess = tf.Session()
+
+
+        self.X = tf.placeholder(tf.int8, shape=(None,) + shape, name='X')
+        self.Y = tf.placeholder(tf.float32, shape=(None, output_size), name='Y')
+
+        units = np.prod(shape)
+
+        input = self.X
+        input = tf.cast(input, tf.float32)
+        input = tf.multiply(input, 1 / 255)
+
+        conv = tf.layers.conv2d(input, 32, [3,3], padding='same', activation=tf.nn.relu)
+        # hidden = createHidden(input, (16,), 'hidden')
+
+        output = createOutput(conv, (output_size,), 'output')
+
+        self.loss = tf.reduce_mean(tf.losses.mean_squared_error(self.Y, output), name='loss')
+        self.run_train = tf.train.AdagradOptimizer(.1).minimize(self.loss)
+
+        self.output = output
 
         self.summary_writer = tf.summary.FileWriter('./graph', self.sess.graph)
         loss_summary = tf.summary.scalar('loss', self.loss)
@@ -79,9 +96,12 @@ class Model:
         feed_dict = {self.X: X, self.Y: Y}
         # start = time.time()
         loss = math.inf
-        while loss > 1:
+        i = 0
+        while loss > .001 and i < 1000:
             loss, _, summary = self.sess.run([self.loss, self.run_train, self.summary], feed_dict=feed_dict)
+            i += 1
             # self.summary_writer.add_summary(summary)
-            print(loss)
+            if i % 100 == 0:
+                print(i, loss)
         # print(time.time() - start)
         return loss
