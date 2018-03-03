a3d9218b67bb16ab6a640d6248b5a192a2fff822
Philip Richardson <uberthought@gmail.com>
2018-03-03 13:37:49 +0000

    many changes

--- imageTest.py
+++ imageTest.py
@@ -3,17 +3,45 @@
 from PIL import Image
 import numpy as np
 import os
+import random
 
 from network import Model
 
-
-def load_images(directory, transform):
+def load_test_images(directory):
     result = np.array([], dtype=np.uint8).reshape(0, Model.size, Model.size, 3)
-    names = os.listdir(directory)
+
+    names = []
+    for root, dirs, files in os.walk(directory, topdown=False):
+        for name in files:
+            names += [os.path.join(root, name)]
     names = [x for x in names if not x.startswith('.')]
+    
     names = sorted(names)
+
     for name in names:
-        image = Image.open(directory + '/' + name)
+        image = Image.open(name)
+        image = image.convert('RGB')
+        image = image.resize((Model.size, Model.size))
+        pixels = np.array(image)
+        result = np.concatenate((result, [pixels]), axis=0)
+
+    return result
+
+def load_train_names(directory):
+    names = []
+
+    for root, dirs, files in os.walk(directory, topdown=False):
+        for name in files:
+            names += [os.path.join(root, name)]
+    names = [x for x in names if not x.startswith('.')]
+    
+    return names
+
+def load_images(names):
+    result = np.array([], dtype=np.uint8).reshape(0, Model.size, Model.size, 3)
+
+    for name in names:
+        image = Image.open(name)
         image = image.convert('RGB')
         image = image.resize((Model.size, Model.size))
 
@@ -21,71 +49,26 @@ def load_images(directory, transform):
         pixels = np.array(image)
         result = np.concatenate((result, [pixels]), axis=0)
 
-        if transform:
-            # 90
-            image = image.rotate(90)
-            pixels = np.array(image)
-            result = np.concatenate((result, [pixels]), axis=0)
-
-            # 180
-            image = image.rotate(90)
-            pixels = np.array(image)
-            result = np.concatenate((result, [pixels]), axis=0)
-
-            # 270
-            image = image.rotate(90)
-            pixels = np.array(image)
-            result = np.concatenate((result, [pixels]), axis=0)
-
-            # flip
-            image = image.rotate(90)
-            image = image.transpose(Image.FLIP_LEFT_RIGHT)
-            pixels = np.array(image)
-
-            # flip 90
-            result = np.concatenate((result, [pixels]), axis=0)
-            image = image.rotate(90)
-            pixels = np.array(image)
-            result = np.concatenate((result, [pixels]), axis=0)
-
-            # flip 180
-            image = image.rotate(90)
-            pixels = np.array(image)
-            result = np.concatenate((result, [pixels]), axis=0)
-
-            # flip 270
-            image = image.rotate(90)
-            pixels = np.array(image)
-            result = np.concatenate((result, [pixels]), axis=0)
-
     return result
 
 model = Model()
 
-training = load_images('training', True)
-testing = load_images('testing', False)
-
 if not os.path.exists('results'):
     os.makedirs('results')
 
-for j in range(100):
+training_names = load_train_names('101_ObjectCategories')
+testing = load_test_images('testing')
+
+for j in range(10):
+    training = load_images(random.sample(training_names, 2))
     loss = model.train(training, testing)
 
     for i in range(testing.shape[0]):
         pixels = testing[i:i + 1:1][0]
         result = model.run([pixels])[0]
-        pixels = np.concatenate([pixels, result], axis=1)
+        diff = np.absolute(pixels.astype('float') - result.astype('float'))
+        diff = diff.astype('uint8')
+        pixels = np.concatenate([pixels, result, diff], axis=1)
         image = Image.fromarray(pixels, "RGB")
-        image = image.resize((256 * 2, 256))
+        image = image.resize((256 * 3, 256))
         image.save('results/image' + str(i).zfill(3) + '.png')
-
-    # pixels = testing[0:1:1][0]
-    # result = model.run([pixels])[0]
-    # diff = np.absolute(pixels - result)
-    # diff = (diff[:, :, 0] + diff[:, :, 1] + diff[:, :, 2]) / 3
-    # diff = np.dstack([diff, diff, diff])
-    # diff = diff.astype('uint8')
-    # pixels = np.concatenate([pixels, result, diff], axis=1)
-    # image = Image.fromarray(pixels, "RGB")
-    # image = image.resize((256 * 3, 256))
-    # image.save('results/image' + str(j).zfill(3) + '.png')
--- network.py
+++ network.py
@@ -4,54 +4,8 @@ import os.path
 import math
 import random
 
-def createConv(input, filters, stride, name):
-    with tf.name_scope(name):
-        size = input.shape.as_list()[1] / stride
-        dimensions = input.shape.as_list()[-1]
-
-        filter3x3 = tf.Variable(tf.truncated_normal([3, 3, dimensions, filters], stddev=1.0 / math.sqrt(float(3 * 3 * dimensions * filters)), dtype=tf.float32), name='filter3x3')
-        # filter5x5 = tf.Variable(tf.truncated_normal([5, 5, dimensions, filters], stddev=1.0 / math.sqrt(float(5 * 5 * dimensions * filters)), dtype=tf.float32), name='filter5x5')
-        # filter7x7 = tf.Variable(tf.truncated_normal([7, 7, dimensions, filters], stddev=1.0 / math.sqrt(float(7 * 7 * dimensions * filters)), dtype=tf.float32), name='filter7x7')
-        biases = tf.Variable(tf.zeros([size, size, filters], dtype=tf.float32), name='biases')
-
-        output3x3 = tf.nn.conv2d(input=input, filter=filter3x3, strides=[1, stride, stride, 1], padding='SAME')
-        # output5x5 = tf.nn.conv2d(input=input, filter=filter5x5, strides=[1, stride, stride, 1], padding='SAME')
-        # output7x7 = tf.nn.conv2d(input=input, filter=filter7x7, strides=[1, stride, stride, 1], padding='SAME')
-
-        output = output3x3 + biases
-
-        # if dimensions == filters:
-        #     if stride == 1:
-        #         pool = input
-        #     if stride == 2:
-        #         pool = tf.nn.pool(input, [stride, stride], "AVG", "VALID", strides=[stride, stride])
-        #     output = output + pool
-
-        output = tf.maximum(output * 0.1, output)
-        # output = tf.nn.relu(output)
-
-        return output
-
-
-def upsample(input, units, name):
-    with tf.name_scope(name):
-        shape = input.get_shape().as_list()
-        dimensions = len(shape[1:-1])
-
-        output = input
-        output = createConv(output, units, 1, 'conv')
-        output = (tf.reshape(output, [-1] + shape[-dimensions:]))
-
-        for i in range(dimensions, 0, -1):
-            output = tf.concat([output, output], i)
-        output_size = [-1] + [s * 2 for s in shape[1:-1]] + [shape[-1]]
-        output = tf.reshape(output, output_size)
-
-        return output
-
-
 class Model:
-    size = 64
+    size = 32
 
     def __init__(self):
 
@@ -62,6 +16,9 @@ class Model:
         self.step = tf.assign(self.global_step, self.global_step + 1)
 
         self.global_loss = tf.Variable(0.2, trainable=False, dtype=tf.float32, name="global_loss")
+        self.last_loss = tf.placeholder(tf.float32, shape=(), name='last_loss')
+        weight = 0.5
+        self.eloss = tf.assign(self.global_loss, self.last_loss * weight + self.global_loss * (1.0 - weight))
 
         self.sess = tf.Session()
 
@@ -73,17 +30,19 @@ class Model:
         layer = input
         original_size = np.prod(layer.get_shape().as_list()[1:])
 
-        outer_units = 16
-        outer_layers = 4
+        outer_units = 4
+        outer_layers = 0
 
-        inner_units = 16
-        inner_layers = 3
+        inner_units = 128
+        inner_layers = 5
+
+        run = "run1"
 
         for i in range(outer_layers):
-            layer = createConv(layer, outer_units, 1, 'conv')
+            layer = self.createConv(layer, outer_units, 1, 'conv')
 
         for i in range(inner_layers):
-            layer = createConv(layer, inner_units, 2, 'conv')
+            layer = self.createConv(layer, inner_units, 2, 'conv')
 
         compressed_size = np.prod(layer.get_shape().as_list()[1:])
         print(original_size, compressed_size, 1 - compressed_size / original_size)
@@ -91,31 +50,30 @@ class Model:
         center_layer = layer
 
         for i in range(inner_layers):
-            layer = upsample(layer, inner_units, 'up')
+            layer = self.upsample(layer, inner_units, 'up')
 
         for i in range(outer_layers):
-            layer = createConv(layer, outer_units, 1, 'conv')
+            layer = self.createConv(layer, outer_units, 1, 'conv')
 
-        layer = createConv(layer, 3, 1, 'conv')
+        layer = self.createConv(layer, 3, 1, 'conv')
 
-        # layer = tf.clip_by_value(layer, 0.0, 1.0)
-        layer = tf.nn.sigmoid(layer)
+        layer = tf.clip_by_value(layer, 0.0, 1.0)
+        # layer = tf.nn.sigmoid(layer)
 
         output = layer
 
         self.output = tf.cast(output * 255, tf.uint8)
 
         self.train_loss = tf.reduce_mean(tf.losses.mean_squared_error(input, output), name='train_loss')
+        self.train_loss2 = tf.sqrt(self.train_loss, name='train_loss2')
         self.run_train = tf.train.AdagradOptimizer(.1).minimize(self.train_loss)
 
-        weight = 0.25
-        self.eloss = tf.assign(self.global_loss, self.train_loss * weight + self.global_loss * (1.0 - weight))
-
         self.test_loss = tf.reduce_mean(tf.losses.mean_squared_error(input, output), name='test_loss')
+        self.test_loss2 = tf.sqrt(self.test_loss, name='test_loss2')
 
-        self.summary_writer = tf.summary.FileWriter('./summary/run2', self.sess.graph)
-        self.train_loss_summary = tf.summary.scalar('train_loss', self.train_loss)
-        self.test_loss_summary = tf.summary.scalar('test_loss', self.test_loss)
+        self.summary_writer = tf.summary.FileWriter('./summary/' + run, self.sess.graph)
+        self.train_loss_summary = tf.summary.scalar('train_loss',self.train_loss2)
+        self.test_loss_summary = tf.summary.scalar('test_loss', self.test_loss2)
         center_layer_summary = tf.summary.histogram('center_layer', center_layer)
         self.summary = tf.summary.merge([self.test_loss_summary, center_layer_summary])
 
@@ -126,6 +84,54 @@ class Model:
             saver = tf.train.Saver()
             saver.restore(self.sess, 'graph/graph')
 
+    def createConv(self, input, filters, stride, name):
+        with tf.name_scope(name):
+            size = input.shape.as_list()[1] / stride
+            dimensions = input.shape.as_list()[-1]
+
+            filter3x3 = tf.Variable(tf.truncated_normal([3, 3, dimensions, filters], stddev=1.0 / math.sqrt(float(3 * 3 * dimensions * filters)), dtype=tf.float32), name='filter3x3')
+            # filter5x5 = tf.Variable(tf.truncated_normal([5, 5, dimensions, filters], stddev=1.0 / math.sqrt(float(5 * 5 * dimensions * filters)), dtype=tf.float32), name='filter5x5')
+            # filter7x7 = tf.Variable(tf.truncated_normal([7, 7, dimensions, filters], stddev=1.0 / math.sqrt(float(7 * 7 * dimensions * filters)), dtype=tf.float32), name='filter7x7')
+            # filter13x13 = tf.Variable(tf.truncated_normal([13, 13, dimensions, filters], stddev=1.0 / math.sqrt(float(13 * 13 * dimensions * filters)), dtype=tf.float32), name='filter13x13')
+            biases = tf.Variable(tf.zeros([size, size, filters], dtype=tf.float32), name='biases')
+
+            output3x3 = tf.nn.conv2d(input=input, filter=filter3x3, strides=[1, stride, stride, 1], padding='SAME')
+            # output5x5 = tf.nn.conv2d(input=input, filter=filter5x5, strides=[1, stride, stride, 1], padding='SAME')
+            # output7x7 = tf.nn.conv2d(input=input, filter=filter7x7, strides=[1, stride, stride, 1], padding='SAME')
+            # output13x13 = tf.nn.conv2d(input=input, filter=filter13x13, strides=[1, stride, stride, 1], padding='SAME')
+
+            output = output3x3 + biases
+
+            output = tf.maximum(output * 0.1, output)
+
+            if dimensions == filters:
+                if stride == 1:
+                    pool = input
+                if stride == 2:
+                    pool = tf.nn.pool(input, [stride, stride], "MAX", "VALID", strides=[stride, stride])
+                    # pool = tf.nn.pool(input, [stride, stride], "AVG", "VALID", strides=[stride, stride])
+                output = output * 0.1 + pool
+
+            return output
+
+
+    def upsample(self, input, units, name):
+        with tf.name_scope(name):
+            shape = input.get_shape().as_list()
+            dimensions = len(shape[1:-1])
+
+            output = input
+            output = self.createConv(output, units, 1, 'conv')
+            output = (tf.reshape(output, [-1] + shape[-dimensions:]))
+
+            for i in range(dimensions, 0, -1):
+                output = tf.concat([output, output], i)
+            output_size = [-1] + [s * 2 for s in shape[1:-1]] + [shape[-1]]
+            output = tf.reshape(output, output_size)
+
+            return output
+
+
     def save(self):
         saver = tf.train.Saver()
         saver.save(self.sess, 'graph/graph')
@@ -135,40 +141,23 @@ class Model:
         return self.sess.run(self.output, feed_dict={self.X: X})
 
     def train(self, training, testing):
-        x = training
-        batch = 4
-        if len(x) > batch:
-            i = np.random.choice(range(len(x)), batch)
-            x = x[i]
-
         i = 0
         train_loss = math.inf
-        last_loss = 0
-        eloss = self.sess.run(self.global_loss, feed_dict={self.X: x})
+        eloss = self.sess.run(self.global_loss)
 
-        while i < 1000 and train_loss > eloss * .95 and train_loss != last_loss:
-            last_loss = train_loss
-            train_loss, _, step = self.sess.run([self.train_loss, self.run_train, self.step], feed_dict={self.X: x})
-            i = i + 1
-
-            if i % 100 == 0:
-                print('training', ((eloss - train_loss) / eloss))
-                if train_loss < 0.25:
-                    self.sess.run([self.eloss], feed_dict={self.X: x})
-                    train_summary = self.sess.run(self.train_loss_summary, feed_dict={self.X: x})
-                    test_summary = self.sess.run(self.summary, feed_dict={self.X: testing})
-                    self.summary_writer.add_summary(train_summary, step)
-                    self.summary_writer.add_summary(test_summary, step)
-                self.save()
-                
-        if train_loss < 0.25 and train_loss != last_loss:
-            self.sess.run([self.eloss], feed_dict={self.X: x})
-            train_summary = self.sess.run(self.train_loss_summary, feed_dict={self.X: x})
-            test_summary = self.sess.run(self.summary, feed_dict={self.X: testing})
+        while i < 1024 and train_loss > eloss * 0.95:
+            
+            i += 1
+            train_loss, _, step, train_summary = self.sess.run([self.train_loss2, self.run_train, self.step, self.train_loss_summary], feed_dict={self.X: training})
             self.summary_writer.add_summary(train_summary, step)
-            self.summary_writer.add_summary(test_summary, step)
+
+        test_loss, test_summary = self.sess.run([self.test_loss2, self.summary], feed_dict={self.X: testing})
+        self.summary_writer.add_summary(test_summary, step)
+            
+        self.sess.run([self.eloss], feed_dict={self.last_loss: train_loss})
+
         self.save()
 
         print('done', eloss, train_loss, ((eloss - train_loss) / eloss))
 
-        return eloss
+        return train_loss
